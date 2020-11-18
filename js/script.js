1c8c5f022c824e3d66cd0fc1736967c3b1cae729
Olia <ostapukolia10@gmail.com>
2020-11-18 18:29:26 +0200

    first function

--- js/script.js
+++ js/script.js
@@ -1,25 +1,11 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let num = 20; 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {}, 
-    genres: [],
-    privat: false
-};
-
-const a = prompt( 'Один из последних просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
-
-if (4 == 4) {
-    console.log('ok');
+function showFirstMessage(text) {
+    console.log(text);
+    num = 10;
 }
+ 
+showFirstMessage('Hello');
+console.log(num);
