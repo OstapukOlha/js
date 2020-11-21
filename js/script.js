2ee4d585f70456f205a906461c6b8661617809ac
Olia <ostapukolia10@gmail.com>
2020-11-21 19:19:02 +0200

    masivu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
 "use strict"; 
 
-const arr = [1, 2, 3, 6, 8];
+const arr = [1, 2, 3, 24, 6, 8];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+// console.log(arr.length);
+
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} hghg ${arr}`)
+// });
 
 // arr.pop();
-arr.push(10);
-console.log(arr);
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join('; '));
